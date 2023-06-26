fd8022aa1fec56be10ccc50809f4443639e8c639
CryptForge <mail.cryptforge@gmail.com>
2023-06-26 13:53:28 +0200

    Update to my fork of minestom-ce

--- src/test/java/io/github/bloepiloepi/pvp/test/PvpTest.java
+++ src/test/java/io/github/bloepiloepi/pvp/test/PvpTest.java
@@ -8,7 +8,6 @@ import io.github.bloepiloepi.pvp.explosion.PvpExplosionSupplier;
 import io.github.bloepiloepi.pvp.legacy.LegacyKnockbackSettings;
 import io.github.bloepiloepi.pvp.potion.effect.CustomPotionEffect;
 import io.github.bloepiloepi.pvp.test.commands.Commands;
-import io.github.togar2.fluids.MinestomFluids;
 import net.kyori.adventure.text.Component;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.attribute.Attribute;
@@ -37,68 +36,66 @@ import net.minestom.server.world.DimensionType;
 import java.util.Optional;
 
 public class PvpTest {
-	public static void main(String[] args) {
-		MinecraftServer server = MinecraftServer.init();
-		PvpExtension.init();
-		MinestomFluids.init();
-		//VelocityProxy.enable("tj7MulOtnIDe");
-		
-		DimensionType fullbright = DimensionType.builder(NamespaceID.from("idk")).ambientLight(1.0f).build();
-		MinecraftServer.getDimensionTypeManager().addDimension(fullbright);
-		
-		Instance instance = MinecraftServer.getInstanceManager().createInstanceContainer(fullbright);
-		instance.setChunkGenerator(new DemoGenerator());
-		instance.enableAutoChunkLoad(true);
-		
-		Pos spawn = new Pos(0, 60, 0);
-		MinecraftServer.getGlobalEventHandler().addListener(PlayerLoginEvent.class, event -> {
-			event.setSpawningInstance(instance);
-			event.getPlayer().setRespawnPoint(spawn);
-			
-			EntityCreature entity = new EntityCreature(EntityType.ZOMBIE);
-			entity.setInstance(instance, spawn);
-			entity.getAttribute(Attribute.MAX_HEALTH).setBaseValue(500);
-			entity.heal();
-			
-			MinecraftServer.getSchedulerManager().buildTask(() -> {
-				Optional<Player> player = MinecraftServer.getConnectionManager()
-						.getOnlinePlayers().stream()
-						.filter(p -> p.getDistanceSquared(entity) < 6)
-						.findAny();
-				
-				if (player.isPresent()) {
-					if (!player.get().damage(CustomDamageType.mob(entity), 1.0F))
-						return;
-					
-					LegacyKnockbackEvent legacyKnockbackEvent = new LegacyKnockbackEvent(player.get(), entity, true);
-					EventDispatcher.callCancellable(legacyKnockbackEvent, () -> {
-						LegacyKnockbackSettings settings = legacyKnockbackEvent.getSettings();
+    public static void main(String[] args) {
+        MinecraftServer server = MinecraftServer.init();
+        PvpExtension.init();
 
-						player.get().setVelocity(player.get().getVelocity().add(
-								-Math.sin(entity.getPosition().yaw() * 3.1415927F / 180.0F) * 1 * settings.extraHorizontal(),
-								settings.extraVertical(),
-								Math.cos(entity.getPosition().yaw() * 3.1415927F / 180.0F) * 1 * settings.extraHorizontal()
-						));
-					});
+        DimensionType fullbright = DimensionType.builder(NamespaceID.from("fullbright")).ambientLight(1.0f).build();
+        MinecraftServer.getDimensionTypeManager().addDimension(fullbright);
+
+        Instance instance = MinecraftServer.getInstanceManager().createInstanceContainer(fullbright);
+        instance.setChunkGenerator(new DemoGenerator());
+        instance.enableAutoChunkLoad(true);
+
+        Pos spawn = new Pos(0, 60, 0);
+        MinecraftServer.getGlobalEventHandler().addListener(PlayerLoginEvent.class, event -> {
+            event.setSpawningInstance(instance);
+            event.getPlayer().setRespawnPoint(spawn);
+
+            EntityCreature entity = new EntityCreature(EntityType.ZOMBIE);
+            entity.setInstance(instance, spawn);
+            entity.getAttribute(Attribute.MAX_HEALTH).setBaseValue(500);
+            entity.heal();
+
+            MinecraftServer.getSchedulerManager().buildTask(() -> {
+                Optional<Player> player = MinecraftServer.getConnectionManager()
+                        .getOnlinePlayers().stream()
+                        .filter(p -> p.getDistanceSquared(entity) < 6)
+                        .findAny();
+
+                if (player.isPresent()) {
+                    if (!player.get().damage(CustomDamageType.mob(entity), 1.0F))
+                        return;
+
+                    LegacyKnockbackEvent legacyKnockbackEvent = new LegacyKnockbackEvent(player.get(), entity, true);
+                    EventDispatcher.callCancellable(legacyKnockbackEvent, () -> {
+                        LegacyKnockbackSettings settings = legacyKnockbackEvent.getSettings();
+
+                        player.get().setVelocity(player.get().getVelocity().add(
+                                -Math.sin(entity.getPosition().yaw() * 3.1415927F / 180.0F) * 1 * settings.extraHorizontal(),
+                                settings.extraVertical(),
+                                Math.cos(entity.getPosition().yaw() * 3.1415927F / 180.0F) * 1 * settings.extraHorizontal()
+                        ));
+                    });
 //					EntityKnockbackEvent entityKnockbackEvent = new EntityKnockbackEvent(player.get(), entity, true, false, 1 * 0.5F);
 //					EventDispatcher.callCancellable(entityKnockbackEvent, () -> {
 //						float strength = entityKnockbackEvent.getStrength();
 //						player.get().takeKnockback(strength, Math.sin(Math.toRadians(entity.getPosition().yaw())), -Math.cos(Math.toRadians(entity.getPosition().yaw())));
 //					});
-				}
-				
-				event.getPlayer().setFood(20);
-			}).repeat(3, TimeUnit.SERVER_TICK).schedule();
-		});
-		
-		MinecraftServer.getGlobalEventHandler().addListener(PlayerSpawnEvent.class, event -> {
-			event.getPlayer().setGameMode(GameMode.CREATIVE);
-			//PvpExtension.setLegacyAttack(event.getPlayer(), true);
-			
-			event.getPlayer().setPermissionLevel(4);
-			event.getPlayer().addEffect(new Potion(PotionEffect.REGENERATION, (byte) 10, CustomPotionEffect.PERMANENT));
-		});
-		
+                }
+
+                event.getPlayer().setFood(20);
+            }).repeat(3, TimeUnit.SERVER_TICK).schedule();
+        });
+
+        MinecraftServer.getGlobalEventHandler().addListener(PlayerSpawnEvent.class, event -> {
+            event.getPlayer().setGameMode(GameMode.CREATIVE);
+            //PvpExtension.setLegacyAttack(event.getPlayer(), true);
+
+            event.getPlayer().setPermissionLevel(4);
+            event.getPlayer().addEffect(new Potion(PotionEffect.REGENERATION, (byte) 10, CustomPotionEffect.PERMANENT));
+        });
+
 //		LegacyKnockbackSettings settings = LegacyKnockbackSettings.builder()
 //				.horizontal(0.35)
 //				.vertical(0.4)
@@ -108,41 +105,39 @@ public class PvpTest {
 //				.build();
 //		MinecraftServer.getGlobalEventHandler().addListener(LegacyKnockbackEvent.class,
 //				event -> event.setSettings(settings));
-		
-		instance.setExplosionSupplier(PvpExplosionSupplier.INSTANCE);
-		
-		GlobalEventHandler eventHandler = MinecraftServer.getGlobalEventHandler();
-		eventHandler.addChild(PvPConfig.defaultBuilder()
-				//.potion(PotionConfig.legacyBuilder().drinking(false))
-				.build().createNode()
-		);
-		
-		eventHandler.addChild(MinestomFluids.events());
-		
-		eventHandler.addListener(PlayerUseItemOnBlockEvent.class, event -> {
-			if (event.getItemStack().material() == Material.WATER_BUCKET) {
-				event.getInstance().setBlock(event.getPosition().relative(event.getBlockFace()), Block.WATER);
-			}
-			event.getPlayer().getInventory().update();
-		});
-		
-		MinecraftServer.getCommandManager().register(new Command("test") {{
-			setDefaultExecutor((sender, args) -> {
-				if (sender instanceof Player player) {
-					player.refreshOnGround(true);
-					player.takeKnockback(0.4f, -0.00825, -0.00716738);
-				}
-			});
-		}});
-		
-		eventHandler.addListener(PlayerTickEvent.class, event -> {
-			event.getPlayer().sendActionBar(Component.text(event.getPlayer().getVelocity().toString()));
-		});
-		
-		Commands.init();
-		
-		OpenToLAN.open();
-		
-		server.start("localhost", 25565);
-	}
+
+        instance.setExplosionSupplier(PvpExplosionSupplier.INSTANCE);
+
+        GlobalEventHandler eventHandler = MinecraftServer.getGlobalEventHandler();
+        eventHandler.addChild(PvPConfig.defaultBuilder()
+                //.potion(PotionConfig.legacyBuilder().drinking(false))
+                .build().createNode()
+        );
+
+        eventHandler.addListener(PlayerUseItemOnBlockEvent.class, event -> {
+            if (event.getItemStack().material() == Material.WATER_BUCKET) {
+                event.getInstance().setBlock(event.getPosition().relative(event.getBlockFace()), Block.WATER);
+            }
+            event.getPlayer().getInventory().update();
+        });
+
+        MinecraftServer.getCommandManager().register(new Command("test") {{
+            setDefaultExecutor((sender, args) -> {
+                if (sender instanceof Player player) {
+                    player.refreshOnGround(true);
+                    player.takeKnockback(0.4f, -0.00825, -0.00716738);
+                }
+            });
+        }});
+
+        eventHandler.addListener(PlayerTickEvent.class, event -> {
+            event.getPlayer().sendActionBar(Component.text(event.getPlayer().getVelocity().toString()));
+        });
+
+        Commands.init();
+
+        OpenToLAN.open();
+
+        server.start("localhost", 25565);
+    }
 }
